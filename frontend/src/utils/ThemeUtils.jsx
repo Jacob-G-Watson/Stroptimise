d20b170e5288cdd8e3548a6be18111b61b34611d
Jacob <jacobwatson100@gmail.com>
2025-09-08 18:06:01 +1000

    fix(ThemeUtils): adjust padding for PrimaryButton and DangerButton for consistency

--- frontend/src/utils/ThemeUtils.jsx
+++ frontend/src/utils/ThemeUtils.jsx
@@ -1,7 +1,7 @@
 export function PrimaryButton({ children, className = "", ...props }) {
 	return (
 		<button
-			className={`px-3 py-1 bg-stropt-green text-white rounded hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-stropt-green ${className}`}
+			className={`px-3 m-1 py-1 bg-stropt-green text-white rounded hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-stropt-green ${className}`}
 			{...props}
 		>
 			{children}
@@ -12,7 +12,7 @@ export function PrimaryButton({ children, className = "", ...props }) {
 export function DangerButton({ children, className = "", ...props }) {
 	return (
 		<button
-			className={`px-2 m-1 py-1 bg-stropt-brown text-white rounded hover:bg-black focus:outline-none focus:ring-2 focus:ring-stropt-brown ${className}`}
+			className={`px-3 m-1 py-1 bg-stropt-brown text-white rounded hover:bg-black focus:outline-none focus:ring-2 focus:ring-stropt-brown ${className}`}
 			{...props}
 		>
 			{children}
